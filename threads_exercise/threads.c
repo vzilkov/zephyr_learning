3b19f639dd7e54b7e84efb4df915891c39119850
vzilkov <vladislav.zilkov@gmail.com>
2025-04-08 13:30:35 +0000

    added address checking

--- threads_exercise/threads.c
+++ threads_exercise/threads.c
@@ -11,14 +11,15 @@ void hello_thread1(void *arg1, void *arg2, void *arg3){
     ARG_UNUSED(arg2);
     ARG_UNUSED(arg3);
     unsigned int count = 0;
+    unsigned int *adr = &count;
     while(1){
         //specially made for the exercise without mutex or 
         // another synchronization methods
-        // printf("Hello, AAAAAAAA thread1! %d\n", ++count);
-        
+        // printf("Hello, AAAAAAAA thread1! %d\n", ++count);   
         char str[] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
         print_function(str, strlen(str));
-        printk("Thread1? %d?\n", ++count);
+        printk("Thread1? %d? 0x%X\n", ++(*adr), adr);
+        k_yield();
     }
 }
 
@@ -27,10 +28,12 @@ void hello_thread2(void *arg1, void *arg2, void *arg3){
     ARG_UNUSED(arg2);
     ARG_UNUSED(arg3);
     unsigned int count = 0;
+    unsigned int *adr = &count;
     while(1){
         // printf("Bye, BBBBBBB thread2! %d\n", ++count);
         char str[] = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
         print_function(str, strlen(str));
-        printk("Thread2! %d!\n", ++count);
+        printk("Thread2! %d! 0x%X\n", ++(*adr), adr);
+        k_yield();
     }
 }
