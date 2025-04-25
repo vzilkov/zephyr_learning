f79acf07ec7fbce3904febc75484d482f4448b8b
Zilkov, Vladislav <vladislav.zilkov@intel.com>
2025-04-25 09:33:09 +0000

    timer implemented in thread 1

--- threads_exercise/threads.c
+++ threads_exercise/threads.c
@@ -6,19 +6,41 @@
 
 #include <string.h>
 
+static struct k_timer timer_thread1;
+#define TIMER_INTERVAL_MS 100
+void timer_expiry_function(struct k_timer *timer_id)
+{
+    char str[] = "Timer expired (str var)!";
+    print_function(str, strlen(str));
+    printk("Timer expired!\n");
+}
+
+// Timer stop function (optional)
+void timer_stop_function(struct k_timer *timer_id)
+{
+    char str[] = "Timer stopped (str var)!";
+    print_function(str, strlen(str));
+    printk("Timer stopped!\n");
+}
+
 void hello_thread1(void *arg1, void *arg2, void *arg3){
     ARG_UNUSED(arg1);
     ARG_UNUSED(arg2);
     ARG_UNUSED(arg3);
     unsigned int count = 0;
     unsigned int *adr = &count;
+
+    k_timer_init(&timer_thread1, timer_expiry_function, timer_stop_function);
+    k_timer_start(&timer_thread1, K_MSEC(TIMER_INTERVAL_MS), K_MSEC(TIMER_INTERVAL_MS));
+
     while(1){
         //specially made for the exercise without mutex or 
         // another synchronization methods
         // printf("Hello, AAAAAAAA thread1! %d\n", ++count);   
         char str[] = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
         print_function(str, strlen(str));
-        printk("Thread1? %d? 0x%X\n", ++(*adr), adr);
+        printk("Thread1? %d? 0x%X\n", ++(*adr), adr);        
+        k_msleep(1000);
         k_yield();
     }
 }
@@ -35,5 +57,6 @@ void hello_thread2(void *arg1, void *arg2, void *arg3){
         print_function(str, strlen(str));
         printk("Thread2! %d! 0x%X\n", ++(*adr), adr);
         k_yield();
+        k_msleep(5000);
     }
 }
